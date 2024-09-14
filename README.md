4c80769d25a3e0b2da7bbab612d7a43eb23ecadf
Eduardo Villota <eduardouio7@gmail.com>
2024-09-14 08:03:40 -0500

    archivos semilla y automatizaciones peques

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+# eliminar migraciones anteriores
+
 # generamos las migraciones
 python manage.py makemigrations accounts
+python manage.py makemigrations equipment
+python manage.py makemigrations projects
+python migrate
+python manage.py sowseed
+
+
--- app/delete_migrations.sh
+++ app/delete_migrations.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
 BASE_DIR=$(pwd)
 
-
 find "$BASE_DIR" -path "*/migrations/*.py" ! -name "__init__.py" -delete
-
-
 find "$BASE_DIR" -path "*/migrations/*.pyc" -delete
+find "$BASE_DIR" -path "*/db.sqlite3" -delete
 
-echo "Carpetas de migrations eliminadas."
+echo "Carpetas de migrations y base sqlite eliminadas."
