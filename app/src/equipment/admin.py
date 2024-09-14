993d15193c006010347e3eeafd366f8e072f36af
Eduardo Villota <eduardouio7@gmail.com>
2024-09-14 07:09:52 -0500

    agregamos los modelos al admin

--- app/src/equipment/admin.py
+++ app/src/equipment/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from .models import Equipment, Vehicle, Supplie, SupplieStockMovment
 
-# Register your models here.
+
+class EquipmentAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class VehicleAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class SupplieAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class SupplieStockMovmentAdmin(SimpleHistoryAdmin):
+    pass
+
+
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(Supplie, SupplieAdmin)
+admin.site.register(SupplieStockMovment, SupplieStockMovmentAdmin)
--- app/src/projects/admin.py
+++ app/src/projects/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+from .models import Mantenance, ChainOfCustody, MantenanceSupplies, Partner, Project
 
-# Register your models here.
+
+class MantenanceAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class ChainOfCustodyAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class MantenanceSuppliesAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class PartnerAdmin(SimpleHistoryAdmin):
+    pass
+
+
+class ProjectAdmin(SimpleHistoryAdmin):
+    pass
+
+
+admin.site.register(Mantenance, MantenanceAdmin)
+admin.site.register(ChainOfCustody, ChainOfCustodyAdmin)
+admin.site.register(MantenanceSupplies, MantenanceSuppliesAdmin)
+admin.site.register(Partner, PartnerAdmin)
+admin.site.register(Project, ProjectAdmin)
