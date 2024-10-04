e4afe9707f118c0ffd65ff4da0ca30eaea1e6683
Eduardo Villota <eduardouio7@gmail.com>
2024-10-04 00:38:40 -0500

    extencion de plantillas

--- app/src/accounts/views/HomeTV.py
+++ app/src/accounts/views/HomeTV.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomeTV(LoginRequiredMixin, TemplateView):
-    template_name = 'base/base.html'
+    template_name = 'pages/home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
