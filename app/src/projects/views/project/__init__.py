ab19d15703726730f5557ac5b27b991a6e13f769
Eduardo Villota <eduardouio7@gmail.com>
2025-06-07 16:57:56 -0500

    Agregar vistas para la gestión de proyectos: crear, actualizar, eliminar y detallar proyectos, incluyendo lógica de contexto y mensajes de acción.

---
+++ app/src/projects/views/project/__init__.py
@@ -0,0 +1,13 @@
+from .ProjectCreateView import ProjectCreateView
+from .ProjectDetailView import ProjectDetailView
+from .ProjectListView import ProjectListView
+from .ProjectUpdateView import ProjectUpdateView
+from .ProjectDeleteView import ProjectDeleteView
+
+__all__ = [
+    'ProjectCreateView',
+    'ProjectDetailView',
+    'ProjectListView',
+    'ProjectUpdateView',
+    'ProjectDeleteView',
+]
