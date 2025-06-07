654a6ccffd0c62b398daeca433f0a156d4184da2
Eduardo Villota <eduardouio7@gmail.com>
2025-06-07 16:20:43 -0500

    Agregar filtros de búsqueda y mejorar la visualización en las listas de técnicos y vehículos; incluir lógica de filtrado en las vistas y actualizar las plantillas correspondientes.

--- app/src/accounts/views/technicals/TechnicalListView.py
+++ app/src/accounts/views/technicals/TechnicalListView.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from django.db.models import Q
 from accounts.models import Technical
 
 
@@ -9,11 +10,75 @@ class TechnicalListView(LoginRequiredMixin, ListView):
     context_object_name = 'technicals'
     ordering = ['first_name']
 
+    def get_queryset(self):
+        queryset = Technical.objects.all().order_by('first_name')
+
+        # Filtros de búsqueda
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.filter(
+                Q(first_name__icontains=search) |
+                Q(last_name__icontains=search) |
+                Q(email__icontains=search) |
+                Q(nro_phone__icontains=search) |
+                Q(dni__icontains=search)
+            )
+
+        # Filtros específicos
+        work_area = self.request.GET.get('work_area')
+        if work_area:
+            queryset = queryset.filter(work_area=work_area)
+
+        is_active = self.request.GET.get('is_active')
+        if is_active:
+            queryset = queryset.filter(is_active=is_active == 'true')
+
+        # Filtros de certificados
+        has_license = self.request.GET.get('has_license')
+        if has_license == 'true':
+            queryset = queryset.filter(license_issue_date__isnull=False)
+        elif has_license == 'false':
+            queryset = queryset.filter(license_issue_date__isnull=True)
+
+        has_defensive_driving = self.request.GET.get('has_defensive_driving')
+        if has_defensive_driving == 'true':
+            queryset = queryset.filter(defensive_driving_certificate_issue_date__isnull=False)
+        elif has_defensive_driving == 'false':
+            queryset = queryset.filter(defensive_driving_certificate_issue_date__isnull=True)
+
+        has_mae_certificate = self.request.GET.get('has_mae_certificate')
+        if has_mae_certificate == 'true':
+            queryset = queryset.filter(mae_certificate_issue_date__isnull=False)
+        elif has_mae_certificate == 'false':
+            queryset = queryset.filter(mae_certificate_issue_date__isnull=True)
+
+        has_medical_certificate = self.request.GET.get('has_medical_certificate')
+        if has_medical_certificate == 'true':
+            queryset = queryset.filter(medical_certificate_issue_date__isnull=False)
+        elif has_medical_certificate == 'false':
+            queryset = queryset.filter(medical_certificate_issue_date__isnull=True)
+
+        return queryset.distinct()
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title_section'] = 'Listado de Técnicos'
         context['title_page'] = 'Listado de Técnicos'
+        
+        # Variables para filtros
+        context['search'] = self.request.GET.get('search', '')
+        context['work_area'] = self.request.GET.get('work_area', '')
+        context['is_active'] = self.request.GET.get('is_active', '')
+        context['has_license'] = self.request.GET.get('has_license', '')
+        context['has_defensive_driving'] = self.request.GET.get('has_defensive_driving', '')
+        context['has_mae_certificate'] = self.request.GET.get('has_mae_certificate', '')
+        context['has_medical_certificate'] = self.request.GET.get('has_medical_certificate', '')
+        
+        context['work_area_choices'] = Technical._meta.get_field('work_area').choices
+        context['status_choices'] = [('true', 'Activo'), ('false', 'Inactivo')]
+        context['certificate_choices'] = [('true', 'Con certificado'), ('false', 'Sin certificado')]
 
+        # Manejo de mensajes de acciones
         if 'action' not in self.request.GET:
             return context
         message = ''
--- app/src/equipment/views/vehicles/VehicleListView.py
+++ app/src/equipment/views/vehicles/VehicleListView.py
@@ -9,22 +9,21 @@ class VehicleListView(ListView):
     model = Vehicle
     template_name = 'lists/vehicle_list.html'
     context_object_name = 'vehicles'
-    paginate_by = 20
-    
+
     def get_queryset(self):
-        queryset = Vehicle.get_all().select_related().prefetch_related(
+        queryset = Vehicle.get_all().select_related().prefetch_related( 
             Prefetch(
                 'certificationvehicle_set',
                 queryset=CertificationVehicle.objects.filter(is_active=True),
                 to_attr='certifications'
             ),
             Prefetch(
-                'passvehicle_set', 
+                'passvehicle_set',
                 queryset=PassVehicle.objects.filter(is_active=True),
                 to_attr='passes'
             )
         )
-        
+
         # Filtros de búsqueda
         search = self.request.GET.get('search')
         if search:
@@ -36,51 +35,34 @@ class VehicleListView(ListView):
                 Q(engine_number__icontains=search) |
                 Q(chassis_number__icontains=search)
             )
-        
+
         # Filtros específicos
         type_vehicle = self.request.GET.get('type_vehicle')
         if type_vehicle:
             queryset = queryset.filter(type_vehicle=type_vehicle)
-            
+
         status_vehicle = self.request.GET.get('status_vehicle')
         if status_vehicle:
             queryset = queryset.filter(status_vehicle=status_vehicle)
-            
+
         owner_transport = self.request.GET.get('owner_transport')
         if owner_transport:
             queryset = queryset.filter(owner_transport=owner_transport)
-        
+
         return queryset.distinct()
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search'] = self.request.GET.get('search', '')
         context['type_vehicle'] = self.request.GET.get('type_vehicle', '')
         context['status_vehicle'] = self.request.GET.get('status_vehicle', '')
-        context['owner_transport'] = self.request.GET.get('owner_transport', '')
-        context['vehicle_types'] = Vehicle._meta.get_field('type_vehicle').choices
-        context['vehicle_statuses'] = Vehicle._meta.get_field('status_vehicle').choices
-        context['owner_choices'] = Vehicle._meta.get_field('owner_transport').choices
-        
-        # Estadísticas mejoradas para la cabecera
-        vehicles = list(context['vehicles'])  # Convertir a lista para evitar múltiples consultas
-        context['total_vehicles'] = len(vehicles)
-        context['active_vehicles'] = sum(1 for v in vehicles if v.is_active)
-        context['peisol_vehicles'] = sum(1 for v in vehicles if v.owner_transport == 'PEISOL')
-        context['contractor_vehicles'] = sum(1 for v in vehicles if v.owner_transport == 'CONTRATANANTE')
-        
-        # Estadísticas de certificaciones y pases
-        context['vehicles_with_certifications'] = sum(1 for v in vehicles if hasattr(v, 'certifications') and v.certifications)
-        context['vehicles_with_passes'] = sum(1 for v in vehicles if hasattr(v, 'passes') and v.passes)
-        
-        # Estadísticas por tipo de vehículo
-        vehicle_type_stats = {}
-        for vehicle in vehicles:
-            vtype = vehicle.get_type_vehicle_display()
-            if vtype in vehicle_type_stats:
-                vehicle_type_stats[vtype] += 1
-            else:
-                vehicle_type_stats[vtype] = 1
-        context['vehicle_type_stats'] = vehicle_type_stats
-        
+        context['owner_transport'] = self.request.GET.get(
+            'owner_transport', '')
+        context['vehicle_types'] = Vehicle._meta.get_field(
+            'type_vehicle').choices
+        context['vehicle_statuses'] = Vehicle._meta.get_field(
+            'status_vehicle').choices
+        context['owner_choices'] = Vehicle._meta.get_field(
+            'owner_transport').choices
+
         return context
