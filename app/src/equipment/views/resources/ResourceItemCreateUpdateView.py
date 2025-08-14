405443ef24ca5dbd244687c41f413b47cb883efa
Eduardo Villota <eduardouio7@gmail.com>
2025-08-14 07:48:19 -0500

    Formulario dinamico

--- app/src/equipment/views/resources/ResourceItemCreateUpdateView.py
+++ app/src/equipment/views/resources/ResourceItemCreateUpdateView.py
@@ -3,9 +3,20 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+import json
 
 from equipment.models import ResourceItem
 from equipment.forms.ResourceItemForm import ResourceItemForm
+from equipment.models.ResourceItem import (
+    SERVICES_FIELDS,
+    LVMNOS_FIELDS,
+    BTSNHM_FIELDS,
+    BTSNMJ_FIELDS,
+    EST4UR_FIELDS,
+    CMPRBN_FIELDS,
+    PTRTAP_FIELDS,
+    PTRTAR_FIELDS,
+)
 
 
 class ResourceItemCreateUpdateView(LoginRequiredMixin, TemplateView):
@@ -31,17 +42,36 @@ class ResourceItemCreateUpdateView(LoginRequiredMixin, TemplateView):
         form = ResourceItemForm(instance=instance)
         context['form'] = form
 
-        # Calcular campos a mostrar (excluir checklist have_*)
-        field_names = [
-            name for name in form.fields.keys() if not name.startswith('have_')
-        ]
+    # 1) Preparar mapa tipo -> campos permitidos
+    # (intersectado con los del formulario)
+        all_form_fields = list(form.fields.keys())
+        full_map = {
+            'SERVIC': SERVICES_FIELDS,
+            'LVMNOS': LVMNOS_FIELDS,
+            'BTSNHM': BTSNHM_FIELDS,
+            'BTSNMJ': BTSNMJ_FIELDS,
+            'EST4UR': EST4UR_FIELDS,
+            'CMPRBN': CMPRBN_FIELDS,
+            'PTRTAP': PTRTAP_FIELDS,
+            'PTRTAR': PTRTAR_FIELDS,
+        }
+        allowed_map = {
+            key: [f for f in fields if f in all_form_fields]
+            for key, fields in full_map.items()
+        }
 
-        # Dividir campos en dos columnas equilibradas
-        mid = (len(field_names) + 1) // 2
-        left_names = field_names[:mid]
-        right_names = field_names[mid:]
+        # 2) Entregar el JSON al template para control dinámico en el cliente
+        context['type_fields_map_json'] = json.dumps(allowed_map)
+        context['initial_type'] = (
+            instance.type_equipment if instance else 'SERVIC'
+        )
 
-        # Nombres (compatibilidad) y BoundFields (uso en template)
+    # 3) Renderizar TODOS los campos (una sola vez)
+    # y dejar que el JS oculte/muestre
+        mid = (len(all_form_fields) + 1) // 2
+        left_names = all_form_fields[:mid]
+        right_names = all_form_fields[mid:]
+
         context['left_fields'] = left_names
         context['right_fields'] = right_names
         context['left_bound_fields'] = [form[name] for name in left_names]
