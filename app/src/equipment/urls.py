975ff1edb9951aa07d96f89b87c932e45703fc78
Eduardo Villota <eduardouio7@gmail.com>
2025-09-18 05:04:00 -0500

    Reorganizar las rutas de vehículos y equipos en urls.py para mejorar la legibilidad

--- app/src/equipment/urls.py
+++ app/src/equipment/urls.py
@@ -14,47 +14,15 @@ from equipment.views import (
 urlpatterns = [
     # Vehicle URLs
     path('vehiculos/', VehicleListView.as_view(), name='vehicle_list'),
-    path(
-        'vehiculos/<int:pk>/',
-        VehicleDetailView.as_view(),
-        name='vehicle_detail',
-    ),
-    path(
-        'vehiculos/crear/',
-        VehicleCreateView.as_view(),
-        name='vehicle_create',
-    ),
-    path(
-        'vehiculos/editar/<int:pk>/',
-        VehicleUpdateView.as_view(),
-        name='vehicle_update',
-    ),
-    path(
-        'vehiculos/eliminar/<int:pk>/',
-        VehicleDeleteView.as_view(),
-        name='vehicle_delete',
-    ),
-    # Resource Item URLs
+    path('vehiculos/<int:pk>/', VehicleDetailView.as_view(), name='vehicle_detail'),
+    path('vehiculos/crear/', VehicleCreateView.as_view(), name='vehicle_create'),
+    path('vehiculos/editar/<int:pk>/', VehicleUpdateView.as_view(), name='vehicle_update'),
+    path('vehiculos/eliminar/<int:pk>/', VehicleDeleteView.as_view(), name='vehicle_delete'),
+    # ResourceItem URLs
     path('equipos/', ResourceItemListView.as_view(), name='resource_list'),
-    path(
-        'equipos/<int:pk>/',
-        ResourceItemDetailView.as_view(),
-        name='resource_detail',
-    ),
-    path(
-        'equipos/crear/',
-        ResourceItemCreateUpdateView.as_view(),
-        name='resource_create',
-    ),
-    path(
-        'equipos/editar/<int:pk>/',
-        ResourceItemCreateUpdateView.as_view(),
-        name='resource_update',
-    ),
-    path(
-        'equipos/eliminar/<int:pk>/',
-        ResourceItemDeleteView.as_view(),
-        name='resource_delete',
-    ),
+    path('equipos/<int:pk>/', ResourceItemDetailView.as_view(), name='resource_detail'),
+    path('equipos/crear/', ResourceItemCreateUpdateView.as_view(), name='resource_create'),
+    path('equipos/editar/<int:pk>/', ResourceItemCreateUpdateView.as_view(), name='resource_update'),
+    path('equipos/eliminar/<int:pk>/', ResourceItemDeleteView.as_view(), name='resource_delete'),
 
 ]
