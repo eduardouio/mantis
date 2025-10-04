f791e370d16e95a4906cebb497551d89313c392a
Eduardo Villota <eduardouio7@gmail.com>
2025-10-04 07:31:07 -0500

    feat: Agregar rutas para la gestión de recursos en proyectos

--- app/src/api/urls.py
+++ app/src/api/urls.py
@@ -15,6 +15,12 @@ from api.technicals import (
 
 from api.resources import UpdateResourceAPI
 
+from api.projects import (
+    AddResourceProjectAPI,
+    DeleteResourceProjectAPI,
+    ResourcesAvailableAPI,
+)
+
 urlpatterns = [
     # vehicles
     path('vehicles/cert_vehicle/', CertVehicleCreateUpdateAPI.as_view(), name='api_cert_vehicle_create_update'),
@@ -28,4 +34,8 @@ urlpatterns = [
     path('technicals/delete_vaccine/', DeleteVaccineAPI.as_view(), name='api_delete_vaccine'),
     # resources
     path('resources/update/', UpdateResourceAPI.as_view(), name='api_update_resource'),
+    # projects
+    path('projects/resources/available', ResourcesAvailableAPI.as_view(), name='api_resources_available'),
+    path('projects/resources/add', AddResourceProjectAPI.as_view(), name='api_add_resource_project'),
+    path('projects/resources/delete', DeleteResourceProjectAPI.as_view(), name='api_delete_resource_project'),
 ]
