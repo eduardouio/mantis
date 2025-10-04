7ee7422df1ed175157b2780b9bb71a8b2a086209
Eduardo Villota <eduardouio7@gmail.com>
2025-10-04 14:23:38 -0500

    feat: Agregar cálculo de totales de renta y mantenimiento en la vista de detalles del proyecto

--- app/src/projects/views/project/ProjectDetailView.py
+++ app/src/projects/views/project/ProjectDetailView.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from projects.models import Project
+from common.AllProjectData import AllProjectData
 
 
 class ProjectDetailView(LoginRequiredMixin, DetailView):
@@ -17,6 +18,22 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
             self.object.partner
         )
 
+        # Obtener todos los datos del proyecto
+        project_data = AllProjectData(self.object.id)
+        context['project_info'] = project_data.get_project_basic_info()
+        context['assigned_equipment'] = project_data.get_assigned_equipment()
+        context['project_sheets'] = project_data.get_all_sheets()
+        
+        # Calcular totales de equipos
+        total_rent = sum(
+            eq['rent_cost'] for eq in context['assigned_equipment']
+        )
+        total_maintenance = sum(
+            eq['maintenance_cost'] for eq in context['assigned_equipment']
+        )
+        context['total_monthly_rent'] = total_rent
+        context['total_monthly_maintenance'] = total_maintenance
+
         if 'action' not in self.request.GET:
             return context
 
@@ -28,7 +45,10 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
         elif context['action'] == 'updated':
             message = 'El proyecto ha sido actualizado con éxito.'
         elif context['action'] == 'no_delete':
-            message = 'No es posible eliminar el proyecto. Existen dependencias.'
+            message = (
+                'No es posible eliminar el proyecto. '
+                'Existen dependencias.'
+            )
         elif context['action'] == 'delete':
             message = 'Esta acción es irreversible. ¿Desea continuar?.'
 
