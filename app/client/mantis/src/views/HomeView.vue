4c9464f5742e561ea0e9e432a58bc7f32083e474
eduardo <eduardouio7@gmail.com>
2025-11-09 15:52:53 -0500

    bautizamos el app

--- app/client/mantis/src/views/HomeView.vue
+++ app/client/mantis/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
+</script>
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-view">
+    <h1>Welcome to Mantis Home View</h1>
+    <p>This is the main dashboard of the Mantis application.</p>
+  </div>
 </template>
