e7f2085968fdbbbeab8500f286e36e488ff2fee4
eduardo <eduardouio7@gmail.com>
2025-11-11 15:41:55 -0500

    mejora la gestión de errores en la API de recursos disponibles y optimiza la serialización de datos

--- app/src/api/projects/ResourcesAvailable.py
+++ app/src/api/projects/ResourcesAvailable.py
@@ -15,30 +15,28 @@ class ResourcesAvailableAPI(View):
 
     def get(self, request):
         """Obtener lista de recursos disponibles."""
-        try:
-            filters = Q(is_active=True, stst_status_disponibility="DISPONIBLE")
-            resources = ResourceItem.objects.filter(filters).order_by(
-                "type_equipment", "code"
-            )
+        filters = Q(is_active=True, stst_status_disponibility="DISPONIBLE")
+        resources = ResourceItem.objects.filter(filters).order_by(
+            "type_equipment", "code"
+        )
+        data = [self._serialize(r) for r in resources]
 
-            data = [self._serialize(r) for r in resources]
-
-            return JsonResponse(
-                {"success": True, "data": data}, 
-                status=200
-            )
-
-        except Exception as e:
-            return JsonResponse({"success": False, "error": str(e)}, status=500)
+        return JsonResponse(
+            {"success": True, "data": data}, 
+            status=200
+        )
 
     def _serialize(self, resource):
         """Serializar ResourceItem a JSON."""
         type_resource = 'SERVICIO' if resource.type_equipment == 'SERVIC' else 'ALQUILER'
+        display_name = f"{type_resource} {resource.name} - {resource.get_type_equipment_display()}"
+        if type_resource == 'SERVICIO':
+            display_name = f"{type_resource} {resource.name}"
         return {
             "id": resource.id,
             "code": resource.code,
             "name": resource.name,
-            "display_name": f" ({type_resource}) {resource.name} - {resource.get_type_equipment_display()}" ,
+            "display_name": display_name,
             "type_equipment": resource.type_equipment ,
             "type_equipment_display": resource.get_type_equipment_display(),
             "brand": resource.brand,
