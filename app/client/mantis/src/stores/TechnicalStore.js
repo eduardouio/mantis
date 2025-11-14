7a982edbbdd573c8a9dbbdf46200f9753998f699
eduardo <eduardouio7@gmail.com>
2025-11-14 12:58:47 -0500

    stores de tecnicos y de vehiculos

---
+++ app/client/mantis/src/stores/TechnicalStore.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+import { appConfig } from "@/AppConfig";
+
+export const useTechnicalStore = defineStore("technicalStore", {
+    state: () => ({
+        technical: {
+
+        },
+        technicals: []
+    }),
+    actions: {
+        async fetchTechnicalsAvailable() {
+            console.log("Fetching available technicals...");
+            try {
+                const response = await fetch(appConfig.URLTechnicalsAvailable, {
+                    method: "GET",
+                    headers: appConfig.headers
+                });
+                if (!response.ok) {
+                    throw new Error("Failed to fetch technicals");
+                }
+                const responseData = await response.json();
+                this.technicals = responseData.data;
+            } catch (error) {
+                console.error("Error fetching technicals:", error);
+            }
+        }
+    }
+});
