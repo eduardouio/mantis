fa47a0d9f539a09aac214520e907301053c1a5a7
eduardo <eduardouio7@gmail.com>
2025-11-15 10:34:04 -0500

    ficha de tecnico

--- app/src/reports/urls.py
+++ app/src/reports/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from reports.views.CustodyChain import CustodyChainReportView
 from reports.views.PDFCustodyChain import PDFCustodyChain
+from reports.views.TechnicalInformationReport import TechnicalInformationReport
 
 app_name = 'reports'
 
 urlpatterns = [
     path('reports/template-custody-chain/<int:id_custody_chain>/', CustodyChainReportView.as_view(), name='custody-chain-report'),
     path('reports/custody-chain/<int:id_custody_chain>/', PDFCustodyChain.as_view(), name='custody-chain-pdf'),
+    path('reports/technical/<int:id>/', TechnicalInformationReport.as_view(), name='technical-information-report'),
 ]
