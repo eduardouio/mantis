b62c084e0901060bf5e16653e002adc81030310e
eduardo <eduardouio7@gmail.com>
2025-11-15 11:30:14 -0500

    refactor de urls

--- app/src/reports/urls.py
+++ app/src/reports/urls.py
@@ -9,10 +9,14 @@ from reports.views.PDFTechnicalVaccineReport import PDFTechnicalVaccineReport
 app_name = 'reports'
 
 urlpatterns = [
+	
+	# Template URLs
     path('reports/template-custody-chain/<int:id_custody_chain>/', CustodyChainReportView.as_view(), name='custody-chain-report'),
-    path('reports/custody-chain/<int:id_custody_chain>/', PDFCustodyChain.as_view(), name='custody-chain-pdf'),
     path('reports/template-technical/<int:id>/', TechnicalInformationReport.as_view(), name='technical-information-report'),
-    path('reports/technical/<int:id>/', PDFTechnicalInformation.as_view(), name='technical-information-pdf'),
     path('reports/template-technical-vaccine/<int:id>/', TechnicalVaccineReport.as_view(), name='technical-vaccine-report'),
+	
+    # PDF URLs
+    path('reports/custody-chain/<int:id_custody_chain>/', PDFCustodyChain.as_view(), name='custody-chain-pdf'),
+    path('reports/technical/<int:id>/', PDFTechnicalInformation.as_view(), name='technical-information-pdf'),
     path('reports/technical-vaccine/<int:id>/', PDFTechnicalVaccineReport.as_view(), name='technical-vaccine-pdf'),
 ]
