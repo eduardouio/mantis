ee1507997603610d68111a4ef2b68e49e996505d
eduardo <eduardouio7@gmail.com>
2026-01-04 09:17:28 -0500

    Reporte general del estado del  equipos

---
+++ app/src/reports/views/EquipmentInfoReport.py
@@ -0,0 +1,32 @@
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from equipment.models.ResourceItem import ResourceItem
+from datetime import datetime
+
+
+class EquipmentInfoReport(TemplateView):
+    template_name = 'reports/equipment_report.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        equipment_id = self.kwargs.get('equipment_id')
+        
+        equipment = get_object_or_404(ResourceItem, id=equipment_id)
+        
+        # Validar que NO sea un servicio
+        if getattr(equipment, 'is_service', False) or equipment.type_equipment == 'SERVIC':
+            context['error'] = 'Este reporte es solo para equipos físicos, no para servicios'
+            return context
+        
+        # Datos básicos del equipo
+        context['equipment'] = equipment
+        context['inspection_date'] = datetime.now().date()
+        context['generated_by'] = self.request.user.get_full_name() or self.request.user.username
+        
+        # Obtener campos organizados
+        context['common_fields'] = equipment.present_common_fields
+        context['specific_fields'] = equipment.present_specific_fields
+        context['have_fields'] = equipment.present_have_fields
+        context['state_fields'] = equipment.present_state_fields
+        
+        return context
