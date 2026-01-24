080e111ac05404d44374c0d4a5c0b296f36b2a67
eduardo <eduardouio7@gmail.com>
2026-01-24 09:44:31 -0500

    agregamos los logs

--- app/src/reports/views/CustodyChain.py
+++ app/src/reports/views/CustodyChain.py
@@ -1,10 +1,15 @@
 from django.views.generic import TemplateView
+from projects.models.CustodyChain import CustodyChain, ChainCustodyDetail
+from common.AppLoggin import loggin_event
 
 
 class CustodyChainReportView(TemplateView):
 	template_name = "reports/chain_custody.html"
 
 	def get_context_data(self, **kwargs):
+		loggin_event(
+			'Mosrando cadena de custodia'
+		)
 		context = super().get_context_data(**kwargs)
 		context['id_custody_chain'] = kwargs.get('id_custody_chain')
 		return context
