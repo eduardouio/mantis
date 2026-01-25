4a77fa34f70972f2677724c9f8ce35e5516c3ce9
eduardo <eduardouio7@gmail.com>
2026-01-25 10:11:20 -0500

    feat: mejorar respuesta de error en la eliminación de recursos con JsonResponse

--- app/src/api/projects/DeleteResourceProject.py
+++ app/src/api/projects/DeleteResourceProject.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views import View
 from projects.models.Project import ProjectResourceItem
 from projects.models.CustodyChain import ChainCustodyDetail
@@ -7,16 +7,15 @@ from projects.models.CustodyChain import ChainCustodyDetail
 class DeleteResourceProjectAPI(View):
     
     def delete(self, request, id_project_resource):
-        # Validar que el recurso existe
         try:
-            project_resource = ProjectResourceItem.objects.get(id=id_project_resource)
+            ProjectResourceItem.objects.get(id=id_project_resource)
         except ProjectResourceItem.DoesNotExist:
             return HttpResponse(status=404)
         
         custody_chain = ChainCustodyDetail.get_by_resource_id(id_project_resource)
         if custody_chain:
-            return HttpResponse(
-                "No se puede eliminar el recurso porque ha sido utilizado en una cadena de custodia.",
+            return JsonResponse(
+                {"error": "No se puede eliminar el recurso porque ha sido utilizado en una cadena de custodia."},
                 status=400
             )
         
