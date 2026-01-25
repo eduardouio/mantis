0235b7ce1646e31569e11a9b58510bd7219e7c4d
eduardo <eduardouio7@gmail.com>
2026-01-25 12:00:32 -0500

    feat: mejorar la legibilidad del código en la API de eliminación de recursos del proyecto

--- app/src/api/projects/DeleteResourceProject.py
+++ app/src/api/projects/DeleteResourceProject.py
@@ -5,25 +5,26 @@ from projects.models.CustodyChain import ChainCustodyDetail
 
 
 class DeleteResourceProjectAPI(View):
-    
+
     def delete(self, request, id_project_resource):
         try:
             project_resource = ProjectResourceItem.objects.get(id=id_project_resource)
         except ProjectResourceItem.DoesNotExist:
             return HttpResponse(status=404)
-        
+
         custody_chain = ChainCustodyDetail.get_by_resource_id(id_project_resource)
         if custody_chain:
             return JsonResponse(
-                {"error": "No se puede eliminar el recurso porque ha sido utilizado en una cadena de custodia."},
-                status=400
+                {
+                    "error": "No se puede eliminar el recurso porque ha sido utilizado en una cadena de custodia."
+                },
+                status=400,
             )
-        
-        # Actualizar el estado de disponibilidad del recurso a DISPONIBLE
+
         resource_item = project_resource.resource_item
-        resource_item.stst_status_disponibility = 'DISPONIBLE'
-        resource_item.save(update_fields=['stst_status_disponibility'])
-        
-        # Eliminar el recurso del proyecto
+        resource_item.stst_status_disponibility = "DISPONIBLE"
+        resource_item.stst_current_project_id = None
+        resource_item.save(update_fields=["stst_status_disponibility"])
+
         ProjectResourceItem.delete_forever(id_project_resource)
         return HttpResponse(status=204)
