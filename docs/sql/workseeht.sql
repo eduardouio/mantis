4331f20c090d55f9379bd888bccfe30f7972f8d8
eduardo <eduardouio7@gmail.com>
2026-02-07 07:36:38 -0500

    base de consultas

--- docs/sql/workseeht.sql
+++ docs/sql/workseeht.sql
@@ -1,1 +1,9 @@
 select * from public.projects_custodychain cc where cc.sheet_project_id = 2;
+
+
+select * from public.projects_project pp where pp.id = 6;
+
+select * from public.projects_projectresourceitem pri where pri.project_id = 6 and pri.id = 67;
+
+
+select * from public.equipment_resourceitem rr where rr.id = 822;
