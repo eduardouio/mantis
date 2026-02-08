067054a9cb92c63851cc7f1d396eb8f1d173a7a4
eduardo <eduardouio7@gmail.com>
2026-02-07 20:29:19 -0500

    agregar clase WorkSheetBuilder y sus importaciones necesarias

---
+++ app/src/common/WorkSheetBuilder.py
@@ -0,0 +1,6 @@
+from projects.models.SheetProject import SheetProject, SheetProjectDetail
+from projects.models.CustodyChain import CustodyChain, ChainCustodyDetail
+from projects.models.Project import ProjectResourceItem
+
+
+class WorkSheetBuilder():
