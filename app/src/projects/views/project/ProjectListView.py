fcc5badd0949d3bfb3b8346c7d0a3cfef93629b3
eduardouio <eduardouio7@gmail.com>
2026-03-07 21:00:15 -0500

    mejoramos la lista de los proyectos acgregando informacion adicional, para poder acceder de mejor manera al proyecto

--- app/src/projects/views/project/ProjectListView.py
+++ app/src/projects/views/project/ProjectListView.py
@@ -1,12 +1,49 @@
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count, Q, Subquery, OuterRef
 from projects.models import Project
+from projects.models.SheetProject import SheetProject
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = 'lists/project_list.html'
     context_object_name = 'projects'
+
+    def get_queryset(self):
+        return Project.objects.annotate(
+            sheets_count=Count(
+                'sheetproject',
+                filter=Q(sheetproject__is_deleted=False),
+            ),
+            equipos_count=Count(
+                'projectresourceitem',
+                filter=Q(
+                    projectresourceitem__type_resource='EQUIPO',
+                    projectresourceitem__is_deleted=False,
+                ),
+            ),
+            servicios_count=Count(
+                'projectresourceitem',
+                filter=Q(
+                    projectresourceitem__type_resource='SERVICIO',
+                    projectresourceitem__is_deleted=False,
+                ),
+            ),
+            has_active_sheet=Count(
+                'sheetproject',
+                filter=Q(
+                    sheetproject__status='IN_PROGRESS',
+                    sheetproject__is_deleted=False,
+                ),
+            ),
+            last_sheet_id=Subquery(
+                SheetProject.objects.filter(
+                    project=OuterRef('pk'),
+                    is_deleted=False,
+                ).order_by('-id').values('id')[:1]
+            ),
+        ).select_related('partner')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
